df321538c7e73690af6df46148466e569cafd987
Alexandre Cesar <acmorcego@gmail.com>
2022-05-29 19:45:57 -0300

    configure and run prettier

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,19 @@ import * as crypto from 'crypto'
 import * as OTP from 'otpauth'
 
 type Account = {
-	name: string,
-	username: string,
-	password: string,
-	iv: string,
-	otpSecret: string,
+	name: string
+	username: string
+	password: string
+	iv: string
+	otpSecret: string
 	otpIV: string
 }
 
 type BrowserAccount = {
-	url: string,
-	username: string,
-	password: string,
-	httpRealm?: string,
+	url: string
+	username: string
+	password: string
+	httpRealm?: string
 	formActionOrigin?: string
 	guid?: string
 	timeCreated?: string
@@ -25,112 +25,119 @@ type BrowserAccount = {
 }
 
 type PasswordGenerationParameters = {
-	includeSymbols: boolean,
+	includeSymbols: boolean
 	length: number
 }
 
 function generateRandomNumberInInterval(min: number, max: number) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 const config = new Conf()
 
 async function run(key: Buffer) {
-
-	const { action } = await prompt({
+	const { action } = (await prompt({
 		type: 'select',
 		name: 'action',
 		message: "Silver train is a comin'",
-		choices: [
-			'my accounts',
-			'import from browser',
-			'exit',
-		]
-	}) as { action: string }
+		choices: ['my accounts', 'import from browser', 'exit'],
+	})) as { action: string }
 	switch (action) {
 		case 'my accounts': {
-			const names = ((config.get('accounts') as Account[]) ?? []).map(({ name }: Account) => name)
-			const { accountName } = await prompt({
+			const names = ((config.get('accounts') as Account[]) ?? []).map(
+				({ name }: Account) => name
+			)
+			const { accountName } = (await prompt({
 				type: 'autocomplete',
 				name: 'accountName',
 				message: 'Choose an account',
-			choices: [ 'new account', ...names, 'exit']
-			}) as { accountName: string }
+				choices: ['new account', ...names, 'exit'],
+			})) as { accountName: string }
 			if (accountName === 'new account') {
-				let loginInformation: Account = await prompt([{
-					type: 'input',
-					name: 'name',
-					message: 'Account name'
-				}, {
-					type: 'input',
-					name: 'username',
-					message: 'Username'
-				}, {
-					type: 'password',
-					name: 'password',
-					message: 'Password - leave it blank for automatic password generation'
-				}])
+				let loginInformation: Account = await prompt([
+					{
+						type: 'input',
+						name: 'name',
+						message: 'Account name',
+					},
+					{
+						type: 'input',
+						name: 'username',
+						message: 'Username',
+					},
+					{
+						type: 'password',
+						name: 'password',
+						message:
+							'Password - leave it blank for automatic password generation',
+					},
+				])
 
 				const iv = crypto.randomBytes(16)
 				const cipher = crypto.createCipheriv('aes256', key, iv)
 
 				if (!loginInformation.password) {
-					const generator = require('secure-random-password');
-					const parameters: PasswordGenerationParameters = await prompt([{
-						type: 'confirm',
-						name: 'includeSymbols',
-						message: 'Include special characters?',
-						initial: true
-					}, {
-						type: 'numeral',
-						name: 'length',
-						message: 'Length of the password',
-						initial: generateRandomNumberInInterval(12,16)
-					}]) as PasswordGenerationParameters
-					loginInformation.password = generator.randomPassword({ 
-						length: parameters.length, 
+					const generator = require('secure-random-password')
+					const parameters: PasswordGenerationParameters = (await prompt([
+						{
+							type: 'confirm',
+							name: 'includeSymbols',
+							message: 'Include special characters?',
+							initial: true,
+						},
+						{
+							type: 'numeral',
+							name: 'length',
+							message: 'Length of the password',
+							initial: generateRandomNumberInInterval(12, 16),
+						},
+					])) as PasswordGenerationParameters
+					loginInformation.password = generator.randomPassword({
+						length: parameters.length,
 						characters: [
 							generator.lower,
 							generator.upper,
-							generator.digits
-						].concat(parameters.includeSymbols ? [ generator.symbols ] : [])
+							generator.digits,
+						].concat(parameters.includeSymbols ? [generator.symbols] : []),
 					})
 				}
 
 				const password = Buffer.concat([
 					cipher.update(loginInformation.password),
-					cipher.final()
+					cipher.final(),
 				])
 
 				config.set('accounts', [
-					...config.get('accounts', []) as Account[],
+					...(config.get('accounts', []) as Account[]),
 					{
 						name: loginInformation.name,
 						username: loginInformation.username,
 						password: password.toString('base64'),
-						iv: iv.toString('base64')
-					}
+						iv: iv.toString('base64'),
+					},
 				])
 				console.log('account saved')
 			} else if (accountName === 'exit') {
 				return false
-			}
-			else {
-				const account = (config.get('accounts') as Account[])
-					.find(({ name }: Account) => name === accountName)!
+			} else {
+				const account = (config.get('accounts') as Account[]).find(
+					({ name }: Account) => name === accountName
+				)!
 
-				const { action } = await prompt({
+				const { action } = (await prompt({
 					type: 'select',
 					name: 'action',
 					message: 'What do',
 					choices: [
 						'Copy password',
-						...(account.otpSecret !== undefined ? ['Copy authentication code'] : []),
+						...(account.otpSecret !== undefined
+							? ['Copy authentication code']
+							: []),
 						'Edit account',
 						'Remove',
-						'Exit'
-					]
-				}) as { action: string }
+						'Exit',
+					],
+				})) as { action: string }
 
 				switch (action) {
 					case 'Copy password': {
@@ -139,7 +146,7 @@ async function run(key: Buffer) {
 						const decipher = crypto.createDecipheriv('aes256', key, iv)
 						const password = Buffer.concat([
 							decipher.update(ciphertext),
-							decipher.final()
+							decipher.final(),
 						]).toString()
 						const clipboard = await import('clipboardy')
 						clipboard.default.writeSync(password)
@@ -151,7 +158,7 @@ async function run(key: Buffer) {
 						const decipher = crypto.createDecipheriv('aes256', key, iv)
 						const secret = Buffer.concat([
 							decipher.update(ciphertext),
-							decipher.final()
+							decipher.final(),
 						]).toString()
 						const code = new OTP.TOTP({ secret }).generate()
 						const clipboard = await import('clipboardy')
@@ -160,112 +167,139 @@ async function run(key: Buffer) {
 						break
 					}
 					case 'Edit account': {
-						const { field } = await prompt({
+						const { field } = (await prompt({
 							type: 'select',
 							name: 'field',
 							message: 'What edit',
-							choices: [
-								'name',
-								'username',
-								'password',
-								'2fa',
-								'nevermind'
-							]
-						}) as { field: string }
+							choices: ['name', 'username', 'password', '2fa', 'nevermind'],
+						})) as { field: string }
 						switch (field) {
-							case ('name'): {
-								const { newName } = await prompt([{
-									type: 'input',
-									name: 'newName',
-									message: `New account name for '${account.name}'`
-								}]) as { newName: string }
-								config.set('accounts', 
-									(config.get('accounts') as Account[])
-										.map(acc => acc.name === account.name ? { ...acc, name: newName } : acc)
+							case 'name': {
+								const { newName } = (await prompt([
+									{
+										type: 'input',
+										name: 'newName',
+										message: `New account name for '${account.name}'`,
+									},
+								])) as { newName: string }
+								config.set(
+									'accounts',
+									(config.get('accounts') as Account[]).map((acc) =>
+										acc.name === account.name ? { ...acc, name: newName } : acc
+									)
 								)
 								break
 							}
-							case ('username'): {
-								const { newUsername } = await prompt([{
-									type: 'input',
-									name: 'newUsername',
-									message: `New username for '${account.name}'`
-								}]) as { newUsername: string }
-								config.set('accounts', 
-									(config.get('accounts') as Account[])
-										.map(acc => acc.name === account.name ? { ...acc, username: newUsername } : acc)
+							case 'username': {
+								const { newUsername } = (await prompt([
+									{
+										type: 'input',
+										name: 'newUsername',
+										message: `New username for '${account.name}'`,
+									},
+								])) as { newUsername: string }
+								config.set(
+									'accounts',
+									(config.get('accounts') as Account[]).map((acc) =>
+										acc.name === account.name
+											? { ...acc, username: newUsername }
+											: acc
+									)
 								)
 								break
 							}
-							case ('password'): {
-								let { newPassword } = await prompt([{
-									type: 'password',
-									name: 'newPassword',
-									message: `New password for '${account.name}' - leave it blank for automatic password generation`
-								}]) as { newPassword: string }
+							case 'password': {
+								let { newPassword } = (await prompt([
+									{
+										type: 'password',
+										name: 'newPassword',
+										message: `New password for '${account.name}' - leave it blank for automatic password generation`,
+									},
+								])) as { newPassword: string }
 								const iv = crypto.randomBytes(16)
 								const cipher = crypto.createCipheriv('aes256', key, iv)
 								if (!newPassword) {
-									const generator = require('secure-random-password');
-									const parameters: PasswordGenerationParameters = await prompt([{
-										type: 'confirm',
-										name: 'includeSymbols',
-										message: 'Include special characters?',
-										initial: true
-									}, {
-										type: 'numeral',
-										name: 'length',
-										message: 'Length of the password',
-										initial: generateRandomNumberInInterval(12,16)
-									}]) as PasswordGenerationParameters
-									newPassword = generator.randomPassword({ 
-										length: parameters.length, 
+									const generator = require('secure-random-password')
+									const parameters: PasswordGenerationParameters =
+										(await prompt([
+											{
+												type: 'confirm',
+												name: 'includeSymbols',
+												message: 'Include special characters?',
+												initial: true,
+											},
+											{
+												type: 'numeral',
+												name: 'length',
+												message: 'Length of the password',
+												initial: generateRandomNumberInInterval(12, 16),
+											},
+										])) as PasswordGenerationParameters
+									newPassword = generator.randomPassword({
+										length: parameters.length,
 										characters: [
 											generator.lower,
 											generator.upper,
-											generator.digits
-										].concat(parameters.includeSymbols ? [ generator.symbols ] : [])
+											generator.digits,
+										].concat(
+											parameters.includeSymbols ? [generator.symbols] : []
+										),
 									})
 								}
 								const newPasswordCipher = Buffer.concat([
 									cipher.update(newPassword),
-									cipher.final()
+									cipher.final(),
 								])
-								config.set('accounts', 
-									(config.get('accounts') as Account[])
-										.map(acc => acc.name === account.name ? { ...acc, password: newPasswordCipher.toString('base64'), iv: iv.toString('base64') } : acc)
+								config.set(
+									'accounts',
+									(config.get('accounts') as Account[]).map((acc) =>
+										acc.name === account.name
+											? {
+													...acc,
+													password: newPasswordCipher.toString('base64'),
+													iv: iv.toString('base64'),
+											  }
+											: acc
+									)
 								)
 								break
 							}
-							case ('2fa'): {
-								const { secret } = await prompt([{
-									type: 'password',
-									name: 'secret',
-									message: `2FA secret for '${account.name}'`
-								}]) as { secret: string }
+							case '2fa': {
+								const { secret } = (await prompt([
+									{
+										type: 'password',
+										name: 'secret',
+										message: `2FA secret for '${account.name}'`,
+									},
+								])) as { secret: string }
 								const totp = new OTP.TOTP({ secret })
-								const { code } = await prompt([{
-									type: 'input',
-									name: 'code',
-									message: 'Authentication code'
-								}]) as { code: string }
-								if (totp.generate() !== code) console.log('Invalid authentication code')
+								const { code } = (await prompt([
+									{
+										type: 'input',
+										name: 'code',
+										message: 'Authentication code',
+									},
+								])) as { code: string }
+								if (totp.generate() !== code)
+									console.log('Invalid authentication code')
 								else {
 									const iv = crypto.randomBytes(16)
 									const cipher = crypto.createCipheriv('aes256', key, iv)
 									const otpSecret = Buffer.concat([
 										cipher.update(secret),
-										cipher.final()
+										cipher.final(),
 									])
-									config.set('accounts', 
-										(config.get('accounts') as Account[])
-											.map(acc =>
-												acc.name === account.name ? {
-													...acc,
-													otpSecret: otpSecret.toString('base64'),
-													otpIV: iv.toString('base64')
-												} : acc
-											)
+									config.set(
+										'accounts',
+										(config.get('accounts') as Account[]).map((acc) =>
+											acc.name === account.name
+												? {
+														...acc,
+														otpSecret: otpSecret.toString('base64'),
+														otpIV: iv.toString('base64'),
+												  }
+												: acc
+										)
 									)
 								}
 							}
@@ -273,14 +307,15 @@ async function run(key: Buffer) {
 						break
 					}
 					case 'Remove': {
-						const { proceed } = await prompt({
+						const { proceed } = (await prompt({
 							type: 'confirm',
 							name: 'proceed',
-							message: 'Are you sure?'
-						}) as { proceed: boolean }
+							message: 'Are you sure?',
+						})) as { proceed: boolean }
 						if (proceed) {
-							const accounts = (config.get('accounts') as Account[])
-								.filter(({ name }) => name !== account.name)
+							const accounts = (config.get('accounts') as Account[]).filter(
+								({ name }) => name !== account.name
+							)
 							config.set('accounts', accounts)
 						}
 						break
@@ -288,53 +323,59 @@ async function run(key: Buffer) {
 					case 'Exit': {
 						return false
 					}
-					default: return
+					default:
+						return
 				}
 			}
 			return true
 		}
 		case 'import from browser': {
-			const { filename } = await prompt({
+			const { filename } = (await prompt({
 				type: 'input',
 				name: 'filename',
-				message: 'What file'
-			}) as { filename: string }
+				message: 'What file',
+			})) as { filename: string }
 			try {
-				const content = (await import('fs')).readFileSync(filename, "utf8")
-				const browserAccounts = (await import('csv-parse/sync')).parse(content, {
-					columns: true,
-					skip_empty_lines: true,
-					delimiter: ','
-				}) as BrowserAccount[]
-				const { prefix } = await prompt({
+				const content = (await import('fs')).readFileSync(filename, 'utf8')
+				const browserAccounts = (await import('csv-parse/sync')).parse(
+					content,
+					{
+						columns: true,
+						skip_empty_lines: true,
+						delimiter: ',',
+					}
+				) as BrowserAccount[]
+				const { prefix } = (await prompt({
 					type: 'input',
 					name: 'prefix',
-					message: 'Want to add a prefix for the imported accounts?'
-				}) as { prefix: string }
-				const importedAccounts = browserAccounts.map(acc => {
+					message: 'Want to add a prefix for the imported accounts?',
+				})) as { prefix: string }
+				const importedAccounts = browserAccounts.map((acc) => {
 					let iv = crypto.randomBytes(16)
 					let cipher = crypto.createCipheriv('aes256', key, iv)
 					let password = Buffer.concat([
 						cipher.update(acc.password),
-						cipher.final()
+						cipher.final(),
 					])
 					let domain = acc.url.match(/^(?:https?:\/\/)?(?:www.)?(.+)/i)?.[1]
-					let suffix = acc.username ? ` (${acc.username})` : ""
+					let suffix = acc.username ? ` (${acc.username})` : ''
 					let name = `${prefix}${domain}${suffix}`
 					return {
 						name: name,
 						username: acc.username,
 						password: password.toString('base64'),
-						iv: iv.toString('base64')
+						iv: iv.toString('base64'),
 					}
 				}) as Account[]
 				config.set('accounts', [
-					...config.get('accounts', []) as Account[],
-					...importedAccounts
+					...(config.get('accounts', []) as Account[]),
+					...importedAccounts,
 				])
-				console.log("Accounts imported successfully")
+				console.log('Accounts imported successfully')
 			} catch (e: any) {
-				console.error(`Could not import accounts from ${filename}: ${e.message}`)
+				console.error(
+					`Could not import accounts from ${filename}: ${e.message}`
+				)
 			}
 			return true
 		}
@@ -346,11 +387,11 @@ async function run(key: Buffer) {
 }
 
 async function init() {
-	const { password } = await prompt({
+	const { password } = (await prompt({
 		type: 'password',
 		name: 'password',
-		message: 'Master password'
-	}) as { password: string }
+		message: 'Master password',
+	})) as { password: string }
 
 	let key
 
@@ -374,7 +415,7 @@ async function init() {
 
 		config.set({
 			salt: salt.toString('base64'),
-			sign
+			sign,
 		})
 
 		console.log('setup complete')
@@ -384,4 +425,3 @@ async function init() {
 }
 
 init()
-
