19cba772051e3d875fc1fd8ed1a7287971222260
Alexandre Cesar <acmorcego@gmail.com>
2022-05-29 23:14:38 -0300

    extract signIn and signUp functions
    - Extrair predicado `hasAccount` para explicitar o propósito do condicional
    - Extrair os trechos de códigos de login e criação da conta para suas próprias funções, `signIn` e `signUp` respectivamente

--- src/signInOrSignUp.ts
+++ src/signInOrSignUp.ts
@@ -2,6 +2,39 @@ import Conf from 'conf/dist/source'
 import { prompt } from 'enquirer'
 import * as crypto from 'crypto'
 
+function signIn(config: Conf, password: string) {
+	const salt = Buffer.from(config.get('salt') as string, 'base64')
+	const key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256')
+
+	const hmac = crypto.createHmac('sha256', key)
+	hmac.update(salt)
+	const sign = hmac.digest('base64')
+
+	if (sign !== config.get('sign')) {
+		console.log('invalid password')
+		return null
+	} else console.log('password ok')
+
+	return key
+}
+
+function signUp(config: Conf, password: string) {
+	const salt = crypto.randomBytes(16)
+	const key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256')
+
+	const hmac = crypto.createHmac('sha256', key)
+	hmac.update(salt)
+	const sign = hmac.digest('base64')
+
+	config.set({
+		salt: salt.toString('base64'),
+		sign,
+	})
+
+	console.log('setup complete')
+	return key
+}
+
 async function signInOrSignUp(config: Conf) {
 	const { password } = (await prompt({
 		type: 'password',
@@ -9,37 +42,9 @@ async function signInOrSignUp(config: Conf) {
 		message: 'Master password',
 	})) as { password: string }
 
-	let key = null
-
-	if (config.has('salt') && config.has('sign')) {
-		const salt = Buffer.from(config.get('salt') as string, 'base64')
-		key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256')
-
-		const hmac = crypto.createHmac('sha256', key)
-		hmac.update(salt)
-		const sign = hmac.digest('base64')
-
-		if (sign !== config.get('sign')) {
-			console.log('invalid password')
-			return null
-		} else console.log('password ok')
-	} else {
-		const salt = crypto.randomBytes(16)
-		key = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256')
-
-		const hmac = crypto.createHmac('sha256', key)
-		hmac.update(salt)
-		const sign = hmac.digest('base64')
-
-		config.set({
-			salt: salt.toString('base64'),
-			sign,
-		})
-
-		console.log('setup complete')
-	}
-
-	return key!
+	const hasAccount = config.has('salt') && config.has('sign')
+	if (hasAccount) return signIn(config, password)
+	return signUp(config, password)
 }
 
 export default signInOrSignUp
